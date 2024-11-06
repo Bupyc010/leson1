cc19325b8ddccfa134324ad13e6d0c683a27d3ee
Ruslan <112889383+Bupyc010@users.noreply.github.com>
2024-11-06 22:03:21 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#1st program
+print(9**0.5*5)
+
+#2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3nd program
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+
+#4nd program
+s = '123.456'
+print(s[s.find('.') + 1])
+print(float(s) * 10)
